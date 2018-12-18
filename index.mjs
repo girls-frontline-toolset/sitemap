4a7bc7a2b17f18d4276c6013bb78819a1a3576c6
andy19974a22 <andy19974a22@gmail.com>
2018-12-18 17:33:03 +0800

    init file

---
+++ index.mjs
@@ -0,0 +1,49 @@
+import common from './modules/Common'
+import fs from 'fs';
+import sm from "sitemap";
+import Process from './modules/Process';
+
+
+let process = new Process();
+let urlsList = [];
+
+process.task01(urlsList);
+
+console.log(urlsList);
+
+console.log(common.$Get);
+
+
+
+
+
+// TODO add girls time list
+
+// noinspection SpellCheckingInspection
+let sitemap = sm.createSitemap({
+    hostname: common.HOST_NAME,
+    cacheTime: 600000,  //600 sec (10 min) cache purge period
+    urls: urlsList
+    // [
+    //     {url: '/', changefreq: 'weekly', priority: 0.8, lastmodrealtime: true, lastmod: today},
+    //     {url: '/page1&=1', changefreq: 'weekly', priority: 0.8, lastmodrealtime: true, lastmod: today},
+    //     {
+    //         url: '/page2', changefreq: 'weekly', priority: 0.8, lastmodrealtime: true, lastmod: today,
+    //         img: [{
+    //             url: '/img1.jpg',
+    //             caption: 'An image',
+    //             title: 'The Title of Image One'
+    //         },],
+    //         links: [
+    //             {lang: 'en', url: 'http://test.com/page-1/',},
+    //             {lang: 'ja', url: 'http://test.com/page-1/ja/',},
+    //             {lang: 'zh-Hans', url: 'http://test.com/page-1/ja/',},
+    //             {lang: 'zh-Hant', url: 'http://test.com/page-1/ja/',},
+    //
+    //
+    //         ]
+    //     }
+    // ]
+});
+
+fs.writeFileSync("./sitemap.xml", sitemap.toString());
