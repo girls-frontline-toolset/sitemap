7e5779db4e39d9fd15b5d27f4100b92c04f6ff6e
andy19974a22 <andy19974a22@gmail.com>
2018-12-20 17:09:59 +0800

    add dollStar type

--- modules/Common.mjs
+++ modules/Common.mjs
@@ -1,5 +1,13 @@
 /**
- * @typedef {array} girlTimeList
+ * @typedef {'5','4','3','2'} dollStar
+ */
+
+/**
+ * @typedef {'HG'|'SMG'|'RF'|'AR'|'MG'|'SG'} dollType
+ */
+
+/**
+ * @typedef  {array} girlTimeList
  * @property {string} time
  * @property {array} star5
  * @property {array} star4
@@ -8,21 +16,28 @@
  */
 
 /**
+ * @namespace girlList
  * @typedef {array} girlList
- * @property {number} no
+ * @property {number} no - no_name
  * @property {string} name
- * @property {'5','4','3','2'} star
- * @property {'HG'|'SMG'|'RF'|'AR'|'MG'|'SG'} type
+ * @property {dollStar} star
+ * @property {dollType} type
  * @property {null|'0'|'1'} heavy
  * @property {string} src
- * @property {null|1} heavy
+ * @property {null|'1'} heavy
  */
+
+if (document.querySelectorAll === null){
+  document.querySelectorAll = function(){
+
+  }
+}
 
 /** @typedef {array} deviceList
  * @property {number} equipment_no
  * @property {string} name
  * @property {string} type
- * @property {'5'|'4'|'3'|'2'} star
+ * @property {dollStar} star
  * @property {string} time
  * @property {string} attribute
  * @property {string} img
